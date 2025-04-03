b7ad68120528604958ef45c661e3882753c51f41
Gaurav 2248 <gaurav.more@nativebyte.in>
2025-04-04 00:40:45 +0530

    added alt to img tag

--- src/components/Card.js
+++ src/components/Card.js
@@ -28,7 +28,8 @@ const Card = (props) => {
   return(
     <div className='w-[300px] bg-bgDark rounded-md overflow-hidden '>
       <div className='relative '> 
-      <img src={course.image.url}/>
+      <img src={course.image.url} alt={course.image.alt || "Course Image"} />
+
       <div className='w-[40px] h-[40px] bg-white rounded-full absolute right-2 bottom-[-12px] grid place-items-center'>
         <button onClick={clickHandler}>
           {
